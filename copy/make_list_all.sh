23b8d8d83f9b9bafc7cf2731b32cce11c7cc55a5
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2016-04-08 13:17:16 -0500

    update script to make list of files. we can now comment out a certain lines.

--- copy/make_list_all.sh
+++ copy/make_list_all.sh
@@ -3,12 +3,13 @@
 IFS=$'\n'
 
 rm *.txt
+grep -v '^#' < directory.list | { 
 while read p; do
     export KEY=`echo $p | awk '{print $2}'`
     export DIR=`echo $p | awk '{print $1}'`
     echo $p
     ./make_list.sh $DIR >& $KEY.txt
-done < directory.list
+done } #< directory.list
 
 eos root://cmseos.fnal.gov rm -r /eos/uscms/store/user/hatake/copy
 eos root://cmseos.fnal.gov mkdir /eos/uscms/store/user/hatake/copy
