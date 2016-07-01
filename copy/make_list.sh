6af07634608d808c1da121c238136625236d5889
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2016-07-01 12:31:07 -0500

    now include date info

--- copy/make_list.sh
+++ copy/make_list.sh
@@ -3,7 +3,7 @@
 export lfnDirectory=$1
 #echo $lfnDirectory $string
 
-export dateStr=`date --date='-60 days' +"%Y-%m-%d %H:%M:%S"`
+export dateStr=`date --date='-365 days' +"%Y-%m-%d %H:%M:%S"`
 timestamp=$(date -d "$dateStr" +%s)
 
 IFS=$'\n'
@@ -14,12 +14,14 @@ for f in `xrdfs root://cmseos.fnal.gov ls -l $(echo $lfnDirectory)`; do
     #echo $f 
     export DATE=`echo $f | awk '{print $2,$3}'`
     export FILE=`echo $f | awk '{print $5}'`
+    #stat -c %Y /eos/uscms/$FILE
+    export curFileStatTime=`stat -c %Y /eos/uscms/$FILE`
     #echo $DATE
     curFileMtime=$(date -d "$DATE" +%s)
     #curFileMtime=$(stat -c %Y "$curFile")
     #xrdfs root://cmseos.fnal.gov ls -l $f | awk '{print $2}'
     #echo $curFileMtime $DATE $timestamp $dateStr
     if (( curFileMtime > timestamp )); then
-        echo $FILE
+        echo $FILE $curFileStatTime
     fi
 done
