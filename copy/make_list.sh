e1fd7600e1a417f374e0eae0a4690647b5e01d89
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2016-07-09 23:44:54 -0500

    use ls only and its timestamp

--- copy/make_list.sh
+++ copy/make_list.sh
@@ -8,21 +8,15 @@ timestamp=$(date -d "$dateStr" +%s)
 
 IFS=$'\n'
 
-#for curFile in *; do
-#for f in $(ls -1); do 
-for f in `xrdfs root://cmseos.fnal.gov ls -l $(echo $lfnDirectory) | grep root`; do
-    #echo $f 
-    export DATE=`echo $f | awk '{print $2,$3}'`
-    export FILE=`echo $f | awk '{print $5}'`
-    #stat -c %Y /eos/uscms/$FILE
-    #export curFileStatTime=`stat -c %Y /eos/uscms/$FILE`
-    #echo $DATE
-    curFileMtime=$(date -d "$DATE" +%s)
-    #echo $curFileStatTime $curFileMtime
-    #curFileMtime=$(stat -c %Y "$curFile")
-    #xrdfs root://cmseos.fnal.gov ls -l $f | awk '{print $2}'
-    #echo $curFileMtime $DATE $timestamp $dateStr
-    if (( curFileMtime > timestamp )); then
-        echo $FILE $curFileMtime
-    fi
-done
+xrdfs root://cmseos.fnal.gov ls -l $(echo $lfnDirectory) | grep root | awk '{split($2,date,"-");split($3,hms,":");time=mktime(date[1] " " date[2] " " date[3] " " hms[1] " " hms[2] " " hms[3]); printf("%s %d\n"),$5,time;}'
+
+#for f in `xrdfs root://cmseos.fnal.gov ls -l $(echo $lfnDirectory) | grep root`; do
+#    export DATE=`echo $f | awk '{print $2,$3}'`
+#    export FILE=`echo $f | awk '{print $5}'`
+#    #stat -c %Y /eos/uscms/$FILE
+#    #export curFileStatTime=`stat -c %Y /eos/uscms/$FILE`
+#    curFileMtime=$(date -d "$DATE" +%s)
+#    if (( curFileMtime > timestamp )); then
+#        echo $FILE $curFileMtime
+#    fi
+#done
--- copy/make_list_kodiak.sh
+++ copy/make_list_kodiak.sh
@@ -10,16 +10,4 @@ timestamp=$(date -d "$dateStr" +%s)
 IFS=$'\n'
 
 ls $pfnDirectory -l --time-style=+"%Y/%m/%d/%H/%M/%S" | grep root | awk -v lfnDir=$lfnDirectory '{split($6,date,"/");time=mktime(date[1] " " date[2] " " date[3] " " date[4] " " date[5] " " date[6]); printf("%s/%s %d\n"),lfnDir,$7,time;}'
-#ls $pfnDirectory | grep root | awk -v lfnDir=$lfnDirectory '{command="date -d" $1 " +%s"; command |getline $1; printf("%s/%s\n"),lfnDir,$1;}'
 
-#for f in `ls -l --time-style=+"%Y-%m-%d %H:%M:%S" $(echo $pfnDirectory) | grep root`; do
-#    export DATE=`echo $f | awk '{print $6,$7}'`
-#    export FILE=`echo $f | awk '{print $8}'`
-#    #export curFileStatTime=`stat -c %Y $pfnDirectory/$FILE`
-#    #curFileMtime=$(date -d "$DATE" +%s)
-#    #echo $DATE $FILE $curFileMtime $curFileStatTime
-#    echo $lfnDirectory'/'$FILE
-#    #if (( curFileMtime > timestamp )); then
-#        #echo $lfnDirectory'/'$FILE $curFileStatTime
-#    #fi
-#done
