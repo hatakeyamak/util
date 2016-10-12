a9d5d7e8ab3dc426c314a2ab856bb0c311fcd8cf
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2016-10-12 01:07:34 -0500

    make it easier to make filelist with certain patters in filenames e.g. SingleMuon, TTJets, WJets, ST_

--- copy/make_list_all.sh
+++ copy/make_list_all.sh
@@ -5,10 +5,15 @@ IFS=$'\n'
 rm *.txt
 grep -v '^#' < directory.list | { 
 while read p; do
-    export KEY=`echo $p | awk '{print $2}'`
-    export DIR=`echo $p | awk '{print $1}'`
-    echo $p
-    ./make_list.sh $DIR >& $KEY.txt
+    export KEY=`echo $p | awk '{print $3}'`  # Selection key word
+    export LIST=`echo $p | awk '{print $2}'` # Name of the file list
+    export DIR=`echo $p | awk '{print $1}'`  # Directory name
+    echo $p $DIR $LIST $KEY 
+    if [ -n "$KEY" ]; then
+	./make_list.sh $DIR | grep $KEY >& $LIST.txt
+    else
+	./make_list.sh $DIR >& $LIST.txt
+    fi
 done } #< directory.list
 
 eos root://cmseos.fnal.gov rm -r /eos/uscms/store/user/hatake/copy
@@ -16,6 +21,3 @@ eos root://cmseos.fnal.gov mkdir /eos/uscms/store/user/hatake/copy
 xrdcp *.txt /eos/uscms/store/user/hatake/copy/.
 xrdcp *.list /eos/uscms/store/user/hatake/copy/.
 
-#for p in `cat directory.txt`; do
-#    echo $p
-#done
