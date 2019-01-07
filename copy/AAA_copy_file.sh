c6df6c06e2325edc57db299dc5428047997a7c7f
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2019-01-07 07:59:25 -0600

    adding new scipt that utilizes https://github.com/FNALLPC/lpc-scripts

---
+++ copy/AAA_copy_file.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ~cmssoft/shrc >& /dev/null
+cd ~/CMSSW_8_0_1/src
+eval `scramv1 runtime -sh`
+cd -
+pwd
+echo 'hostname' `hostname`
+
+while read line; do
+  echo "$line"
+  python copyfiles.py T3_US_FNALLPC $line local '/cms/data//store/user/lpcsusyhad/'$line -p gfal -su lpcsusyhad -t 864000
+done < $1
