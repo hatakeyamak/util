1524eb4c22f957b394ed7acf2bfb68a81715dc39
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2020-04-18 17:52:40 -0500

    Add files via upload

---
+++ scripts/run_lumi_info.py
@@ -0,0 +1,73 @@
+# import ROOT in batch mode
+import sys
+import re
+#oldargv = sys.argv[:]
+#sys.argv = [ '-b-' ]
+#sys.argv = oldargv
+import ROOT
+from ROOT import TF1, TF2, TH1, TH2, TH2F, TProfile, TAxis, TMath, TEllipse, TStyle, TFile, TColor, TSpectrum, TCanvas, TPad, TVirtualFitter, gStyle
+ROOT.gROOT.SetBatch(True)
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+print sys.argv[0],sys.argv[1],sys.argv[2]
+#from ctypes import c_uint8
+
+# load FWLite C++ libraries
+ROOT.gSystem.Load("libFWCoreFWLite.so")
+ROOT.gSystem.Load("libDataFormatsFWLite.so")
+ROOT.FWLiteEnabler.enable()
+
+#
+ROOT.gROOT.SetStyle('Plain') # white background
+H_NEvents = ROOT.TH2F ("NEvents","NEvents",201,-2.5,1002.5, 201,-2.5,1002.5)
+H_NEvents_Pass = ROOT.TH2F ("NEvents_Pass","NEvents_Pass",201,-2.5,1002.5, 201,-2.5,1002.5)
+
+# load FWlite python libraries
+from DataFormats.FWLite import Handle, Lumis, Events
+
+genFilter, genFilterLabel = Handle("<GenFilterInfo>"), "genFilterEfficiencyProducer"
+generator, generatorLabel = Handle("<GenLumiInfoHeader>"), "generator"
+
+# open file (you can use 'edmFileUtil -d /store/whatever.root' to get the physical file name)
+
+input_file_list  = sys.argv[1]+'_'+sys.argv[2]+'_list.txt'
+text_file = open(input_file_list, "r")
+list = [line.rstrip() for line in text_file.readlines()]
+print list
+lumiblocks = Lumis(list)
+
+output_ROOT_file_name  = sys.argv[1]+'_'+sys.argv[2]+'_nevents.root'
+output_file            = ROOT.TFile(output_ROOT_file_name, 'recreate')
+
+for ilumi,lumiblock in enumerate(lumiblocks):
+    #if ilumi>100: break
+    lumiblock.getByLabel(genFilterLabel, genFilter)
+    lumiblock.getByLabel(generatorLabel, generator)
+
+    genFilterInfo = genFilter.product()
+    generatorInfo = generator.product()
+    items = re.split('_+', generatorInfo.configDescription())
+    print generatorInfo.configDescription(),genFilterInfo.sumPassWeights(),genFilterInfo.sumWeights()
+    print items[0],items[1],items[2]
+    mMother=items[1]
+    mDaughter=items[2]
+    if items[0]=="TChiWH":
+        mMother=items[3]
+        mDaughter=items[4]
+        print mMother,mDaughter
+    
+    H_NEvents.Fill(float(mMother),float(mDaughter),genFilterInfo.sumWeights());
+    H_NEvents_Pass.Fill(float(mMother),float(mDaughter),genFilterInfo.sumPassWeights());
+
+# Set up canvas : 
+w =  700 
+h =  700
+can  = ROOT.TCanvas("can", "histograms   ", w, h)
+
+H_NEvents.Draw("colz")
+can.SaveAs("test.pdf")
+
+output_file.cd()
+H_NEvents.Write()
+H_NEvents_Pass.Write()
